9156a067c45d79abb9e3a73b64e7cb8a900b8f4b
Steve Burkett <steve@burkettnet.com>
2019-05-29 17:03:10 -0700

    Add auto scaling group to aws-ruby

---
+++ spec/auto_scaling_group_spec.rb
@@ -0,0 +1,16 @@
+require 'spec_helper'
+
+RSpec.describe OpenStax::Aws::AutoScalingGroup do
+  let(:double_asg) { double(desired_capacity: 2) }
+
+  subject(:asg) { described_class.new(name:'foo', region: 'bar') }
+
+  before do
+    allow(Aws::AutoScaling::AutoScalingGroup).to receive(:new).and_return(double_asg)
+  end
+
+  it 'creates the auto scaling group' do
+    expect(double_asg).to receive(:set_desired_capacity).with({desired_capacity: 6})
+    asg.increase_desired_capacity(by: 4)
+  end
+end
