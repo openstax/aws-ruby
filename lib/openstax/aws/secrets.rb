2edb59cf834f9b118e850188dbacadbfe2864bbe
PiotrKozlowski <piotr@obrandowani.pl>
2020-06-12 15:58:33 +0200

    Fix typos in secrets.rb

--- lib/openstax/aws/secrets.rb
+++ lib/openstax/aws/secrets.rb
@@ -66,7 +66,7 @@ module OpenStax::Aws
         OpenStax::Aws.logger.info("Secrets did not change")
         return false
       else
-        OpenStax::Aws.logger.info("Updating the following secrets in the AWS parameter store: #{changed_secrets}")
+        OpenStax::Aws.logger.info("Updating the following secrets in the AWS parameter store: #{@changed_secrets}")
 
         # Ship 'em
         if !dry_run
@@ -119,7 +119,7 @@ module OpenStax::Aws
         end
 
         # Keep the old value around in case we need to revert it
-        new_secrets[:old_value] = existing_secret[:value]
+        new_secret[:old_value] = existing_secret ? existing_secret[:value] : nil
 
         array.push(new_secret)
       end
