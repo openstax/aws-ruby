af5dc22990cc9603a9e1725fa70bb55d8b7bb2c3
scb6 <scb6@rice.edu>
2021-02-24 15:46:04 -0600

    debugging

--- lib/openstax/aws/git_helper.rb
+++ lib/openstax/aws/git_helper.rb
@@ -1,5 +1,6 @@
 require 'git'
 require 'open-uri'
+require 'byebug'
 
 module OpenStax::Aws
   module GitHelper
@@ -8,11 +9,17 @@ module OpenStax::Aws
       ::Git.ls_remote("https://github.com/#{org_slash_repo}")["branches"][branch][:sha]
     end
 
-    def self.file_content_at_sha(org_slash_repo:, sha:, path:, github_token:nil )
+    def self.file_content_at_sha(org_slash_repo:, sha:, path:, github_token: nil )
+      byebug
       if github_token.blank?
         location = "https://raw.githubusercontent.com/#{org_slash_repo}/#{sha}/#{path}"
       else
-        location = "https://raw.githubusercontent.com/#{org_slash_repo}/#{sha}/#{path}?token=#{github_token}"
+        Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+          request = Net::HTTP::Get.new uri.request_uri
+           request.basic_auth 'token', github_token
+           response = http.request request
+           location = response.body
+        end
       end
       file = open(location)
       file.read
