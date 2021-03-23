7cad09a1892763fd7fba29624b9a5a818fb09cdf
Scott Batchelder <33845092+scb6@users.noreply.github.com>
2021-03-23 14:57:46 -0500

    adding functionality for a private repo (#93)
    * adding functionality for a private repo
    
    * addressing comments
    
    * debugging
    
    * updating the logic
    
    * forgot URI
    
    * forgot URI
    
    * Version bump
    
    Co-authored-by: Dante Soares <dante.m.soares@rice.edu>

--- lib/openstax/aws/git_helper.rb
+++ lib/openstax/aws/git_helper.rb
@@ -8,11 +8,20 @@ module OpenStax::Aws
       ::Git.ls_remote("https://github.com/#{org_slash_repo}")["branches"][branch][:sha]
     end
 
-    def self.file_content_at_sha(org_slash_repo:, sha:, path:)
-      location = "https://raw.githubusercontent.com/#{org_slash_repo}/#{sha}/#{path}"
-      file = open(location)
-      file.read
+    def self.file_content_at_sha(org_slash_repo:, sha:, path:, github_token: nil )
+      if github_token.blank?
+        location = "https://raw.githubusercontent.com/#{org_slash_repo}/#{sha}/#{path}"
+        file = open(location)
+        file.read
+      else
+        uri = URI("https://raw.githubusercontent.com/#{org_slash_repo}/#{sha}/#{path}")
+        Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https') do |http|
+          request = Net::HTTP::Get.new uri.request_uri
+           request.basic_auth 'token', github_token
+           response = http.request request
+           response.body
+        end
+      end
     end
-
   end
 end
