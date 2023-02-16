32497a65c1a513f5879581197f29144d0fcddb1b
Dante Soares <dante.m.soares@rice.edu>
2023-02-16 14:29:05 -0600

    Fixed test failure in newer version of rspec

--- spec/rds_instance_spec.rb
+++ spec/rds_instance_spec.rb
@@ -4,8 +4,7 @@ RSpec.describe OpenStax::Aws::RdsInstance do
   let(:instance) { described_class.new(db_instance_identifier: "foo", region: "us-east-1") }
 
   it "changes the master password" do
-    expect(instance.raw).to receive(:modify).with(apply_immediately: true, master_user_password: "bar")
+    expect(instance.raw).to receive(:modify).with({apply_immediately: true, master_user_password: "bar"})
     instance.set_master_password(password: "bar")
   end
 end
-
